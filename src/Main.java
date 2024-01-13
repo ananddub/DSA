00cfbdbe31106a44ced840294eb4d97c85d1726e
ananddub <133890942+ananddub@users.noreply.github.com>
2024-01-14 04:53:14 +0530

    StringBuilder
    Signed-off-by: ananddub <133890942+ananddub@users.noreply.github.com>

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] arg) {
+        char[] arr = {'e', 'e', 'e', 'e', 'e', 'e', 'n', 'n', 'n', 'n'};
+        int[] arrs = {1, 2, 3, 4, 5, 6};
+//            System.out.println(Arrays.toString(new FLP().searchRange(arrs,2)));
+        System.out.println(Infinite.binary(arrs, 4));
+    }
+
+}
